d5501b0dd7a1cee563deb860b6fbbf86ede64c77
richard3251 <paladog31@naver.com>
2024-12-05 11:06:27 +0900

    run, r

---
+++ src/main/java/com/ll/SimpleDb.java
@@ -0,0 +1,14 @@
+package com.ll;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SimpleDb {
+    private final String host;
+    private final String username;
+    private final String password;
+    private final String dbName;
+
+
+
+}
