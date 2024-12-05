5a9ecd2a2f915616dfb86aa3c8abce959f6b2b3f
richard3251 <paladog31@naver.com>
2024-12-05 14:39:03 +0900

    beforeEach, r

--- src/main/java/com/ll/SimpleDb.java
+++ src/main/java/com/ll/SimpleDb.java
@@ -15,8 +15,11 @@ public class SimpleDb {
     private final String dbName;
 
     private Connection getConnection() throws SQLException {
-        // JDBC URL 구성
+        // JDBC URL을 구성
+        // "jdbc:mysql://호스트/데이터베이스이름?옵션들" 형태로 작성
         String url = String.format("jdbc:mysql://%s/%s?useSSL=false&serverTimezone=UTC", host, dbName);
+
+        // DriverManager를 사용해 데이터베이스와 연결된 Connection 객체를 생성
         return DriverManager.getConnection(url, username, password);
     }
 
--- src/test/java/com/ll/simpleDb/SimpleDbTest.java
+++ src/test/java/com/ll/simpleDb/SimpleDbTest.java
@@ -3,6 +3,8 @@ package com.ll.simpleDb;
 import com.ll.SimpleDb;
 import org.junit.jupiter.api.*;
 
+import java.util.stream.IntStream;
+
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class SimpleDbTest {
     private static SimpleDb simpleDb;
@@ -12,6 +14,12 @@ public class SimpleDbTest {
         simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
 
         createArticleTable();
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        truncateArticleTable();
+        makeArticleTestData();
     }
 
     private static void createArticleTable() {
@@ -28,6 +36,27 @@ public class SimpleDbTest {
                     isBlind BIT(1) NOT NULL DEFAULT 0
                 )
                 """);
+    }
+
+    private void makeArticleTestData() {
+        IntStream.rangeClosed(1, 6).forEach(no -> {
+            boolean isBlind = no > 3;
+            String title = "제목%d".formatted(no);
+            String body = "내용%d".formatted(no);
+
+            simpleDb.run("""
+                    INSERT INTO article
+                    SET createdDate = NOW(),
+                    modifiedDate = NOW(),
+                    title = ?,
+                    `body` = ?,
+                    isBlind = ?
+                    """, title, body, isBlind);
+        });
+    }
+
+    private void truncateArticleTable() {
+        simpleDb.run("TRUNCATE article");
     }
 
     @Test
